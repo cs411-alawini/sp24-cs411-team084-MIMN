aefe1d017a30e2a01a76f8b4b6b52d390532b908
qicui3 <qicui3@illinois.edu>
2024-04-30 00:21:14 -0500

    added css

--- app/server.js
+++ app/server.js
@@ -44,7 +44,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '../public'));
+app.use(express.static(__dirname + 'public'));
 
 const accountRoutes = require('./routes/accounts');
 app.use('/accounts', accountRoutes);

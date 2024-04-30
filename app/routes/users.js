fb8fb3af63f91edd0d4c1fa0ee411fb4c18c8106
qicui3 <qicui3@illinois.edu>
2024-04-29 22:49:51 -0500

    fixed user post app

--- app/routes/users.js
+++ app/routes/users.js
@@ -10,6 +10,14 @@ const connection = mysql.createConnection({
 
 connection.connect;
 
+function generateUserId(username) {
+  const now = Date.now().toString();
+  const random = Math.random().toString();
+  const hash = crypto.createHash('sha256').update(username + now + random).digest('hex');
+  const number = parseInt(hash.substring(0, 10), 16);
+  return number % 10000000000;
+}
+
 router.get('/', (req, res) => {
   const user_id = '00023e5e59'
   var sql = 'SELECT user_id, username, email, gre_q, gre_v, gre_awa, gpa, status, dream_area FROM user WHERE user_id = ?';
@@ -27,26 +35,18 @@ router.get('/', (req, res) => {
   });
 });
 
-function getUserInfoAndRender(user_id, res) {
-  var sql = 'SELECT username, email, gre_q, gre_v, gre_awa, gpa, status, dream_area FROM user WHERE user_id = ?';
-  connection.query(sql, [user_id], function(err, result) {
-    if (err) {
-      res.status(500).send({ message: 'Error fetching user info', error: err });
-    } else if (result.length === 0) {
-      res.status(404).send({ message: 'User not found' });
-    } else {
-      res.render('account', { title: 'Account Information', userInfo: result[0] });
-    }
-  });
-}
-
 router.post('/post', express.urlencoded({ extended: true }), (req, res) => {
-  const { user_id, username, email, password, gre_q, gre_v, gre_awa, gpa, status, dream_area } = req.body;
+  const { degree, term, gre_q, gre_v, gre_awa, gpa, status, 
+    university, decision, date, user_id  } = req.body;
 
-  var sql = `INSERT INTO user (user_id, username, email, password, gre_q, gre_v, gre_awa, gpa, status, dream_area)
-             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+  const applicationId = generateUserId(username);
 
-  connection.query(sql, [user_id, username, email, password, gre_q, gre_v, gre_awa, gpa, status, dream_area], function(err, result) {
+  var sql = `INSERT INTO application (user_id, application_id, degree, term, decision, gre_q, gre_v, gre_awa, 
+    gpa, status, university, decision_date)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+
+  connection.query(sql, [user_id, applicationId, degree, term, decision, 
+    gre_q, gre_v, gre_awa, gpa, status, university, date], function(err, result) {
     if (err) {
       console.error('Error adding new user:', err);
       res.status(500).send({ message: 'Error adding new user', error: err });

a3a3ffffce054eff6e1bf9b41eb88524ce8862bc
qicui3 <qicui3@illinois.edu>
2024-04-30 01:56:20 -0500

    fixed profile post

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -11,18 +11,43 @@ const connection = mysql.createConnection({
 connection.connect;
 
 router.get('/', (req, res) => {
-    if (req.session.user) {
-      res.render('profile', { title: 'Account Create' });
-    } else {
-      res.render('profile', { title: 'Account Create' });
-    }
+    res.render('profile', { title: 'Account Create' });
 });
 
 router.post('/', express.urlencoded({ extended: true }), async (req, res) => {
-    const { gpa, gre_v, gre_q, status } = req.body;
-    console.log("Received data:", { gpa, gre_v, gre_q, status });
+    let { gpa, gre_v, gre_q, gre_awa, status } = req.body;
 
-    res.send("Data received");
+    switch (status) {
+        case "inter_wo_us":
+            status = "International without US degree";
+            break;
+        case "inter_w_us":
+            status = "International with US degree";
+            break;
+        case "american":
+            status = "American";
+            break;
+        default:
+            status = "Unknown";
+    }
+
+    const user_id = req.session.user.id;
+
+    gpa = gpa ? parseFloat(gpa) : null;
+    gre_q = gre_q ? parseInt(gre_q, 10) : null;
+    gre_v = gre_v ? parseInt(gre_v, 10) : null;
+    gre_awa = gre_awa ? parseInt(gre_awa, 10) : null;
+
+    sql = "UPDATE user SET gpa = ?, gre_q = ?, gre_v = ?, gre_awa = ?, status = ? WHERE user_id = ?";
+
+    connection.query(sql, [gpa, gre_q, gre_v, gre_awa, status, user_id], function(err, result) {
+        if (err) {
+            console.error('Error adding new profile:', err);
+            res.status(500).send({ message: 'Error adding new profile', error: err });
+        } else {
+            res.redirect('/')
+        }
+    });
 });
 
 module.exports = router;

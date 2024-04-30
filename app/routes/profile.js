23dbbbe6358ce8a91262a255f3411795e2d52497
qicui3 <qicui3@illinois.edu>
2024-04-30 01:23:20 -0500

    added profile creation

---
+++ app/routes/profile.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: '35.232.135.106',
+    user: 'root',
+    password: 'UIUC-cs411-MIMN',
+    database: 'COLLEGE_DB'
+});
+
+connection.connect;
+
+router.get('/', (req, res) => {
+    if (req.session.user) {
+      res.redirect('/');
+    } else {
+      res.render('profile', { title: 'Account Create' });
+    }
+});
+
+module.exports = router;
